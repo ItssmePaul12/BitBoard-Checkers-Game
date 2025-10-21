6dc64a03140a4c24445d7249d2009f8b2192c7f7
Paul <therche1234@gmail.com>
2025-10-20 22:40:13 -0400

    Added many comments for proper documentation.

--- bitops.c
+++ bitops.c
@@ -1,24 +1,27 @@
 //Used an AI tool to help me the undeerstanding of the code.
 #include "bitops.h"
 
-
+//This function is setting the bit at a specified position, which goes to 1.
 void SetBit(unsigned long long *value, int position){
     *value |= (1ULL << position);
 }
 
-
+//This function is clearing, which will set to zero, at the specified position.
 void ClearBit(unsigned long long *value, int position){
     *value &= ~(1ULL << position);
 }
 
+//This function is meant for toggling the bits of the board game.
 void ToggleBit(unsigned long long *value, int position){
     *value ^= (1ULL << position);
 }
 
+//This is meant for returning the value of a bit, either 0 or 1, at a specified position.
 int GetBit(unsigned long long value, int position){
     return (value >> position) & 1ULL;
 }
 
+//This function is meant for counting the total calculation of the bits.
 int CountBits(unsigned long long value){
     int count = 0;
     while (value){
--- makefile
+++ makefile
@@ -1,20 +1,30 @@
+# Used AI tool to help me get a better understanding of the code.
+
+# These set of lines are meant for compiling and flaging purposes.
 CC = gcc
 CFLAGS = -Wall -Wextra -std=c11 -g
+
+# These set of lines are meant for the sourcing files and targeting purposes.
 SRC = main.c bitops.c game.c
 OBJ = $(SRC:.c=.o)
 TARGET = checkers
 
+# This line is meant for the default location for building the target.
 all: $(TARGET)
 
+# These set of lines are for linking the object files, which will go to execution for final.
 $(TARGET): $(OBJ)
 	$(CC) $(CFLAGS) -o $(TARGET) $(OBJ)
 
+# These set of lines are meant for compiling the .c files into object files (.o).
 %.O: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
+# This is meant for running the board game.
 run: all
 	./$(TARGET)
 
+# This is meant for cleaning any files that have been generated.
 clean:
 	rm -f $(OBJ) $(TARGET)
 
